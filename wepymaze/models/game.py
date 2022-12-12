611ffea4dbb61c78a5e33a9fed24a15a3b0f0ceb
Thierry Chappuis <tchappui@gmail.com>
2022-12-12 17:45:54 +0100

    Refactored config

--- wepymaze/models/game.py
+++ wepymaze/models/game.py
@@ -1,16 +1,15 @@
 """Module définissant la facade du modèle représentant le jeu."""
 
-from config.settings import BASE_DIR
+from wepymaze.settings import BASE_DIR
 from .maze import Maze
 from .hero import Hero
 
 
 class Game:
-
     def __init__(self):
         """Initialise le modèle de l'application."""
         self.hero = Hero()
-        self.maze = Maze(BASE_DIR/'levels'/'level-01.txt')
+        self.maze = Maze(BASE_DIR / 'levels' / 'level-01.txt')
         self.maze.add(self.hero)
 
     def move(self, direction):
